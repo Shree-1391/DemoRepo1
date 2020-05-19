f78e53c2d5b7eb6a3a0245fdd6d5824dc2f04386
Shree-1391 <nagashree.r54@gmail.com>
2020-05-20 00:13:42 +0530

    added one line

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,8 +1,11 @@
 
 public class SampleTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello");
+		
+		System.out.println("Java programming");
 	}
 
 }
